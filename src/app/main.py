6d391696badd17a23ad53d2962008fcc5c3989b5
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-08-31 18:01:52 -0400

    feat: presigned URL for uploading and downloading

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,24 @@
 import json
+import S3
 
 def handler(event, context):
     httpMethod = event['httpMethod']
     path = event['path']
 
+
+    
+    if httpMethod == 'GET' and path == '/':
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'message': 'Hello, world!'
+            })
+        }
+
+    
+
     return {
-        'statusCode': 200,
+        'statusCode': 400,
         'body': json.dumps({
             'httpMethod': httpMethod,
             'path': path
